0ef8e602cd247d29e0ca9ab8a52524c24312f9af
anthonycasarta <anthonycasarta@gmail.com>
2024-11-17 14:04:38 -0600

    fix Styles.css to styles.css

--- client/src/components/landingPage/AppointmentsPage.jsx
+++ client/src/components/landingPage/AppointmentsPage.jsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import '../../Styles.css';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "../../styles.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Appointments({}) {
-
-    return(
-        <h>test</h>
-
-    )
-
+  return <h>test</h>;
 }
