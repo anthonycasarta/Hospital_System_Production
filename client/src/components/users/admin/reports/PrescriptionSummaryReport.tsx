6a43685bdac719f9d5955556bf476fd9a61074f4
anthonycasarta <anthonycasarta@gmail.com>
2024-11-17 22:29:37 -0600

    asdaw

--- client/src/components/users/admin/reports/PrescriptionSummaryReport.tsx
+++ client/src/components/users/admin/reports/PrescriptionSummaryReport.tsx
@@ -82,41 +82,40 @@ export default function PrescriptionSummaryReport() {
     },
   });
 
-  const fetchPrescriptions = async () => {
-    try {
-      console.log("BEFORE");
-      const token = localStorage.getItem("token");
-
-      console.log("token: ", token);
-
-      if (!token) {
-        throw new Error("No token found");
-      }
-      console.log("filters: ", filters);
-      const response = await axios.get(
-        `${envConfig.apiUrl}/auth/admin/get-prescription-report`,
-
-        {
-          params: { filters: filters, activeTab: activeTab },
-          withCredentials: true,
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      console.log("response pres report: ", response.data.data);
-      setFilterOptions(response.data.data);
-      setPrescriptions(response.data.data);
-      setFilteredPrescriptions(response.data.data);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error fetching prescriptions:", error);
-      setLoading(false);
-    }
-  };
-
   useEffect(() => {
+    const fetchPrescriptions = async () => {
+      try {
+        console.log("BEFORE");
+        const token = localStorage.getItem("token");
+
+        console.log("token: ", token);
+
+        if (!token) {
+          throw new Error("No token found");
+        }
+        console.log("filters: ", filters);
+        const response = await axios.get(
+          `${envConfig.apiUrl}/auth/admin/get-prescription-report`,
+
+          {
+            params: { filters: filters, activeTab: activeTab },
+            withCredentials: true,
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        console.log("response pres report: ", response.data.data);
+        setFilterOptions(response.data.data);
+        setPrescriptions(response.data.data);
+        setFilteredPrescriptions(response.data.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching prescriptions:", error);
+        setLoading(false);
+      }
+    };
     fetchPrescriptions();
   }, [activeTab, filters]);
 
